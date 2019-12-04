4b8f36774fcc35b8b0c34a974da78a51d585f276
Branden <b.lowe333@gmail.com>
2019-12-04 15:58:11 -0800

    day4 part 2 complete

--- goSolutions/day4.go
+++ goSolutions/day4.go
@@ -9,17 +9,22 @@ const (
 
 func main() {
 	validPasswordCount := 0
-	for i := min + 1; i < max; i++ {
-		if validInput(i) {
+	validPasswordCountAdv := 0
+	for i := min; i <= max; i++ {
+		if validInputPart1(i) {
 			validPasswordCount++
+		}
+		if validInputPart2(i) {
+			validPasswordCountAdv++
 		}
 	}
 
-	fmt.Println(validPasswordCount)
+	fmt.Println("part 1:", validPasswordCount)
+	fmt.Println("part 2:", validPasswordCountAdv)
 
 }
 
-func validInput(num int) bool {
+func validInputPart1(num int) bool {
 	revNum := intToSliceRev(num)
 	hasAdjency := false
 	for i := 0; i < len(revNum)-1; i++ {
@@ -29,6 +34,35 @@ func validInput(num int) bool {
 		if revNum[i] == revNum[i+1] {
 			hasAdjency = true
 		}
+	}
+	return hasAdjency
+}
+
+func validInputPart2(num int) bool {
+	revNum := intToSliceRev(num)
+	var prev int
+	count := 0
+	hasAdjency := false
+	for i, n := range revNum {
+		if i == 0 {
+			prev = n
+			count = 1
+			continue
+		}
+		if n > prev {
+			return false
+		} else if prev == n {
+			count++
+		} else {
+			if count == 2 {
+				hasAdjency = true
+			}
+			prev = n
+			count = 1
+		}
+	}
+	if count == 2 {
+		hasAdjency = true
 	}
 	return hasAdjency
 }
